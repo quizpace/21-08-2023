0ba3cba383008c368de3e96bf98d36e9c4704643
Avi <drazninavi@gmail.com>
2023-08-23 22:17:34 +0300

    up

---
+++ utils.js
@@ -0,0 +1,10 @@
+// function max_ran_array(array) {
+//     const highestNumber = Math.max(...array);
+//     document.write(`<br /> highest number: ${highestNumber}<br />`);
+// }
+
+// function min_ran_array(array) {
+//     const smallestNumber = Math.min(...array);
+//     document.write(`<br /> Smallest number: ${smallestNumber}<br />`);
+// }
+
